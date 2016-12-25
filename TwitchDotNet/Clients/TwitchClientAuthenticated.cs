91410ed310b506ac38cb0179b20fbacc6b01a32b
Travis Liew <contact@travisliew.com>
2016-12-26 08:55:10 +1100

    Switched project to .NET Core (UWP)

--- TwitchDotNet/Clients/TwitchClientAuthenticated.cs
+++ TwitchDotNet/Clients/TwitchClientAuthenticated.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TwitchDotNet.Enums;
 using TwitchDotNet.Helpers;
 using TwitchDotNet.Interfaces;
 
@@ -12,8 +13,117 @@ namespace TwitchDotNet.Clients {
     /// Authenticated TwitchClient
     /// Provides an interface for an authenticated session to retrieve data from Twitch API's (where authentication is required)
     /// </summary>
-    class TwitchClientAuthenticated : TwitchClientUnauthenticated, ITwitchClientUnauthenticated {
-        
-        
+    class TwitchClientAuthenticated : TwitchClientUnauthenticated, ITwitchClientAuthenticated {
+        public dynamic BlockUser(string _userId, string _targetUserId) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic CheckChannelSubscriptionByUser(string _channelId, string _targetUserId) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic CheckUserSubscriptionByChannel(string _userId, string _channelId) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic CreateChannelFeedPost(string _channelId, string _content, bool _share = false) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic CreateChannelFeedPostComment(string _channelId, string _postId, string _content) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic CreateReactionToChannelFeedPost(string _channelId, string _postId, string _emoteId) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic CreateReactionToChannelFeedPostComment(string _channelId, string _postId, string _commentId, string _emoteId) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic DeleteChannelFeedPost(string _channelId, string _postId) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic DeleteChannelFeedPostComment(string _channelId, string _postId, string _commentId) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic DeleteReactionToChannelFeedPost(string _channelId, string _postId, string _emoteId) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic DeleteReactionToChannelFeedPostComment(string _channelId, string _postId, string _commentId, string _emoteId) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic FollowChannel(string _userId, string _targetChannelId, bool _enableNotifications = false) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetChannel() {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetChannelEditors(string _channelId) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetChannelFeedPost(string _channelId, string _postId, long _comments = 5) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetChannelFeedPostComments(string _channelId, string _postId, Pagination _pagination = null) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetChannelFeedPosts(string _channelId, Pagination _pagination = null, long _comments = 5) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetChannelSubscribers(string _channelId, Pagination _pagination = null, SortDirection _direction = SortDirection.asc) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetFollowedStreams(Pagination _pagination = null, StreamType _streamType = StreamType.live) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetFollowedVideos(Pagination _pagination = null, string _game = null, Period _period = Period.week, BroadcastType _broadcastType = BroadcastType.highlight) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetUser() {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetUserBlockList(string _userId, Pagination _pagination = null) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetUserEmotes(string _userId) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic ResetChannelStreamKey(string _channelId) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic StartChannelCommercial(string _channelId) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic UnblockUser(string _userId, string _targetUserId) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic UnfollowChannel(string _userId, string _targetChannelId) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic UpdateChannel(string _channelId, string _status, string _game, string _delay, bool _channelFeedEnabled) {
+            throw new NotImplementedException();
+        }
     }
 }
--- TwitchDotNet/Clients/TwitchClientUnauthenticated.cs
+++ TwitchDotNet/Clients/TwitchClientUnauthenticated.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TwitchDotNet.Interfaces;
+using TwitchDotNet.Enums;
+using TwitchDotNet.Helpers;
 
 namespace TwitchDotNet.Clients {
 
@@ -13,6 +15,96 @@ namespace TwitchDotNet.Clients {
     /// Provides an interface for an unauthenticated session to retrieve data from Twitch API's (where authentication is not required)
     /// </summary>
     internal class TwitchClientUnauthenticated : ITwitchClientUnauthenticated {
-        
+        public dynamic CheckUserFollowsByChannel(string _userId, string _channelId) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetChannelById(string _channelId) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetChannelFollowers(string _channelId, Pagination _pagination = null, SortDirection _direction = SortDirection.desc) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetChannelTeams(string _channelId) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetChannelVideos(string _channelId, Pagination _pagination = null, BroadcastType _broadcastType = BroadcastType.highlight, List<string> _language = null, SortBy _sort = SortBy.time) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetChatBadgesByChannel(string _channelId) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetChatBadgesBySet(string _emoteSets = null) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetEmoticons() {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetFeaturedStreams(Pagination _pagination = null) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetIngests() {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetStream(string _channelId, StreamType _streamType = StreamType.live) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetStreams() {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetStreamsSummary(string _game = null) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetTeam(string _teamName) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetTeams(Pagination _pagination = null) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetTopGames(Pagination _pagination = null) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetTopVideos(Pagination _pagination = null, string _game = null, Period _period = Period.week, BroadcastType _broadcastType = BroadcastType.highlight) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetUser(string _userId) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetUserFollowedChannels(string _userId, Pagination _pagination = null, SortDirection _direction = SortDirection.desc, SortKey _sortBy = SortKey.created_at) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetVideo(string _videoId) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic SearchChannels(string _query, Pagination _pagination = null) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic SearchGames(string _query, string _type = "suggest", bool _live = false) {
+            throw new NotImplementedException();
+        }
+
+        public dynamic SearchStreams(string _query, Pagination _pagination = null, bool _hls = true) {
+            throw new NotImplementedException();
+        }
     }
 }
