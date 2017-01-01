66c5ad771ac72890f0bd785611f815d0d038675a
Travis Liew <contact@travisliew.com>
2017-01-01 15:13:18 +1100

    Updated package and assembly info

--- TwitchDotNet.Tests/Properties/AssemblyInfo.cs
+++ TwitchDotNet.Tests/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TwitchDotNet.Tests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Testing class for .NETCore (UWP) C# Library for Twitch API v5")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TwitchDotNet.Tests")]
